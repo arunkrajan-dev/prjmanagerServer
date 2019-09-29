964204aa604f1844f0e5aaa896f15b8584bd9d8e
arun <arunsugan08@gmail.com>
2019-09-29 11:46:20 -0400

    Added CRUD User Routest and Controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,7 +4,35 @@ exports.test = function (req, res) {
     res.send('Greetings from the Test controller!');
 };
 
-exports.user_create = function (req, res, next) {
+exports.user_details = function (req, res) {
+    User.findById(req.params.id, function (err, user) {
+        if (err) return next(err);
+        res.send(user);
+    })
+};
+
+exports.user_list = function (req, res) {
+    var userList = User.find();
+
+    var queryParms = req.query;
+
+    if (queryParms.searchKey) {
+        userList.or([
+            { 'firstName': { $regex: queryParms.searchKey, $options: 'i' } },
+            { 'lastName': { $regex: queryParms.searchKey, $options: 'i' } }]);
+    }
+
+    if (queryParms.sortKey) {
+        userList.sort([[queryParms.sortKey, 1]]);
+    }
+
+    userList.exec(function (err, users) {
+        if (err) return next(err);
+        res.send(users);
+    });
+};
+
+exports.user_create = function (req, res) {
     let user = new User(
         req.body
     );
@@ -14,5 +42,26 @@ exports.user_create = function (req, res, next) {
             return next(err);
         }
         res.send('Created New User for employee Id -  ' + req.body.employeeId);
+    })
+};
+
+exports.user_details = function (req, res) {
+    User.findById(req.params.id, function (err, user) {
+        if (err) return next(err);
+        res.send(user);
+    })
+};
+
+exports.user_update = function (req, res) {
+    user.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, user) {
+        if (err) return next(err);
+        res.send('Updated User!');
+    });
+};
+
+exports.user_delete = function (req, res) {
+    user.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Deleted User!');
     })
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -6,4 +6,8 @@ const user_controller = require('../controllers/user.controller');
 router.get('/test', user_controller.test);
 module.exports = router;
 
+router.get('/', user_controller.user_list);
 router.post('/create', user_controller.user_create);
+router.get('/:id', user_controller.user_details);
+router.put('/:id/update', user_controller.user_update);
+router.delete('/:id/delete', user_controller.user_delete);
